7d7d42d83005ad66ef7826412908d5936021128a
João Matheus <jm2matheus@gmail.com>
2023-06-23 12:44:12 -0300

    UI melhorada

---
+++ Assets/GameOver/Script/ButtonsGameOver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsGameOver : MonoBehaviour
+{
+    //variavel que recebe o menu principal 
+    [SerializeField] private string mainMenu;
+    
+    //variavel que recebe o game
+    [SerializeField] private string game;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //reinicia o game
+    public void Restart()
+    {
+        SceneManager.LoadScene(game);
+    }
+    
+    //volta para o menu principal
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
--- Assets/Player/Scripts/LoseScript.cs
+++ Assets/Player/Scripts/LoseScript.cs
@@ -6,6 +6,12 @@ public class LoseScript : MonoBehaviour
 {
     [SerializeField] private GameObject Spawn; 
     
+    //variavel que recebe o menu de game over
+    [SerializeField] private GameObject gameOver;
+    
+    //variavel que recebe o menu da interface
+    [SerializeField] private GameObject interfacePlayer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +33,31 @@ public class LoseScript : MonoBehaviour
             other.GetComponent<MoveObstacle>().enabled = false;
             GetComponent<Moviment>().enabled = false;
             GetComponent<Rigidbody2D>().mass = 0;
+            //desativa interface
+            interfacePlayer.SetActive(false);
+            //ativa menu de game over
+            gameOver.SetActive(true);
+        }
+        
+        //se colidir com o obstaculo bone e o player se mover
+        else if (other.gameObject.layer == 9)
+        {
+            //mudar depois para mobile //////////////////////////
+            //Touch touch = Input.GetTouch(0);
+            //se o player segurar a tela do celular ele agacha
+            //if (touch.phase == TouchPhase.Stationary || Input.GetKeyDown(KeyCode.S))
+            if (GetComponent<Rigidbody2D>().velocity.y >= 1 || GetComponent<Rigidbody2D>().velocity.y <= -1 || Input.GetKey(KeyCode.S))
+            {
+                //desativa scripts após colisão do player
+                Spawn.GetComponent<SpawnObstacle>().enabled = false;
+                other.GetComponent<MoveObstacle>().enabled = false;
+                GetComponent<Moviment>().enabled = false;
+                GetComponent<Rigidbody2D>().mass = 0;
+                //desativa interface
+                interfacePlayer.SetActive(false);
+                //ativa menu de game over
+                gameOver.SetActive(true);
+            }
         }
     } 
 }
